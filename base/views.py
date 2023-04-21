3287ccea09bc81990422d278cb7b5001c697b27c
xjama <jamshidbekollanazarov@outlook.com>
2023-04-21 22:20:32 +0500

    send data front end

--- base/views.py
+++ base/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 
+programmers = [
+    {"id": 1, "name": "Anton", "description": "Anton is a project manager for developing"},
+    {"id": 2, "name": "Asal", "description": "Asal is a project manager for developing"},
+    {"id": 3, "name": "Loran", "description": "Loran is a project manager for developing"}
+]
+
 def home(request):
     return render(request, "home.html")
 
 def about(request):
-    return render(request, "about.html")
+    return render(request, "about.html", {'programmers': programmers})

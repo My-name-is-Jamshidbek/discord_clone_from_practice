a7595a8ab744414f5bd1789b747dcd25a2a863d9
xjama <jamshidbekollanazarov@outlook.com>
2023-04-21 20:38:03 +0500

    tested with basic parameters and functions

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # personal applications
+    
+    'base.apps.BaseConfig'
+    
 ]
 
 MIDDLEWARE = [

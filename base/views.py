1a9c309ac92f8caced9d05376315f169cab7a27c
xjama <jamshidbekollanazarov@outlook.com>
2023-04-21 22:00:53 +0500

    add templates in html file

--- base/views.py
+++ base/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse("This is home page")
+    return render(request, "home.html")
 
 def about(request):
-    return HttpResponse("This is about page")
+    return render(request, "about.html")

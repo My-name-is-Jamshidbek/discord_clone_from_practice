461a48af66f43eedccf8016884004a5eff2d90c9
xjama <jamshidbekollanazarov@outlook.com>
2023-04-22 11:58:33 +0500

    view and model connected

--- base/views.py
+++ base/views.py
@@ -1,22 +1,19 @@
 from django.shortcuts import render
+from .models import Room
 
 # Create your views here.
 
-rooms_data = [
-    {"id": 1, "name": "Anton", "description": "Anton is a project manager for developing"},
-    {"id": 2, "name": "Asal", "description": "Asal is a project manager for developing"},
-    {"id": 3, "name": "Loran", "description": "Loran is a project manager for developing"}
-]
 
 def home(request):
     return render(request, "base/home.html")
 
 def rooms(request):
+    rooms_data = Room.objects.all()
     context = {'rooms': rooms_data}
 
     return render(request, "base/rooms.html", context)
 
 def room(request, pk):
-    room_data = next(filter(lambda i: i['id'] == pk, rooms_data), None)
+    room_data = Room.objects.get(id=pk)
     context = {'room': room_data}
     return render(request, "base/room.html", context)

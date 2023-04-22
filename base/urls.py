445236b1ebaa0c616186b077e94985944331b7fe
xjama <jamshidbekollanazarov@outlook.com>
2023-04-22 11:44:24 +0500

    --about and abouts changed room and rooms --writed models ----Message ----Room ----Topic

--- base/urls.py
+++ base/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("abouts/", views.abouts, name="abouts"),
-    path("about/<int:pk>/", views.about, name="about"),
+    path("rooms/", views.rooms, name="rooms"),
+    path("room/<int:pk>/", views.room, name="room"),
 ]
--- base/views.py
+++ base/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 
-programmers = [
+rooms_data = [
     {"id": 1, "name": "Anton", "description": "Anton is a project manager for developing"},
     {"id": 2, "name": "Asal", "description": "Asal is a project manager for developing"},
     {"id": 3, "name": "Loran", "description": "Loran is a project manager for developing"}
@@ -11,11 +11,12 @@ programmers = [
 def home(request):
     return render(request, "base/home.html")
 
-def abouts(request):
-    context = {'programmers': programmers}
-    return render(request, "base/abouts.html", context)
+def rooms(request):
+    context = {'rooms': rooms_data}
 
-def about(request, pk):
-    programmer = next(filter(lambda i: i['id'] == pk, programmers), None)
-    context = {'programmer': programmer}
-    return render(request, "base/about.html", context)
+    return render(request, "base/rooms.html", context)
+
+def room(request, pk):
+    room_data = next(filter(lambda i: i['id'] == pk, rooms_data), None)
+    context = {'room': room_data}
+    return render(request, "base/room.html", context)

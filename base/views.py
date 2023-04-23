fb9844a380ef8dd3039c3ff420d15357f2b00893
xjama <jamshidbekollanazarov@outlook.com>
2023-04-23 22:08:18 +0500

    request.user show in home page

--- base/views.py
+++ base/views.py
@@ -49,6 +49,7 @@ def register(request):
             user = form.save(commit=False)
             user.username = user.username.lower()
             user.save()
+            login(request, user)
             return redirect('home')
         
     context = {'form': form}

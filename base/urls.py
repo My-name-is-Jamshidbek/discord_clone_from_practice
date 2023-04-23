a0126a1e3a7bb8486ede968de7424f5d224e9a46
xjama <jamshidbekollanazarov@outlook.com>
2023-04-23 22:04:31 +0500

    sorted

--- base/urls.py
+++ base/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.home, name="home"),
     path('login/', views.login, name='login'),
     path('register/', views.register, name='register'),
     path('logout/', views.logout, name='logout'),
-    path("", views.home, name="home"),
     path("rooms/", views.rooms, name="rooms"),
     path("room/<int:pk>/", views.room, name="room"),
     path("create-room/", views.create_room, name="create-room"),
